ccd3bbe18538af3f5a95fe2727bb4725c9f15f95
nklapste <nklapste@ualberta.ca>
2017-07-31 21:37:32 -0400

    refactoring

--- webdep/__init__.py
+++ webdep/__init__.py
@@ -10,13 +10,13 @@ import os
 # DIRECTORY GLOBALS
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
 
-# PUBLIC FOLDERS
+# PUBLIC GLOBALS
 PUBDIR = os.path.join(BASEDIR, "public")
 IMGDIR = os.path.join(PUBDIR, "images")
 SCPDIR = os.path.join(PUBDIR, "scripts")
 CONDIR = os.path.join(PUBDIR, "config")
 
-# WEBPAGES FOLDERS
+# WEBPAGES GLOBALS
 WEBDIR = os.path.join(BASEDIR, "webpages")
 
 # LOGS GLOBALS
